2f0a1064d91d9f0951b37bc4d6e6a34738052aff
NguyenThoQuan <thoquan150903@gmail.com>
2024-05-30 13:49:46 +0700

    Final UI

--- src/Components/BuildPC.jsx
+++ src/Components/BuildPC.jsx
@@ -1,61 +1,63 @@
 import React from "react";
 import "../Styles/BuildPC.scss";
 
-export default function BuildPC({ handleBuildPC, money, selectedOption }) {
+export default function BuildPC({ handleStartOver, money, selectedOption }) {
     return (
         <div className="buildPC">
             <div className="button">
-                <button onClick={handleBuildPC}>Start over</button>
+                <button onClick={handleStartOver}>Start over</button>
             </div>
-            <div className="type-pc">
-                <span>{selectedOption}</span>
-            </div>
-            <div className="form-build-PC">
-                <span>CPU</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>GPU</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>Motherboard</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>RAM</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>Cooler</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>PSU</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>Storage</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="form-build-PC">
-                <span>Case</span>
-                <span>PC</span>
-                <span>PC</span>
-            </div>
-            <div className="total-money">
-                <span>Total</span>
-                <span>{money} $</span>
+            <div className="info">
+                <div className="type-pc">
+                    <span>{selectedOption}</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>CPU</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>GPU</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>Motherboard</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>RAM</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>Cooler</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>PSU</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>Storage</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="form-build-PC">
+                    <span>Case</span>
+                    <span>PC</span>
+                    <span>PC</span>
+                </div>
+                <div className="total-money">
+                    <span>Total</span>
+                    <span>{money} $</span>
+                </div>
             </div>
             <div className="button">
-                <button onClick={handleBuildPC}>Start over</button>
+                <button onClick={handleStartOver}>Start over</button>
             </div>
         </div>
     )
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CustomerBuildPC from "./CustomerBuildPC";
-import "../Styles/Home.scss"
 
 export default function Home() {
     return (
